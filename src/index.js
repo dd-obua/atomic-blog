9c22e5241d7d1ce341e81e2a192f9b78869338db
Obua Denis Daniel <dd.obua>
2024-05-13 13:49:55 +0300

    Import App into index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import AppMemo from "./AppMemo";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <AppMemo />
+    <App />
   </StrictMode>
 );
